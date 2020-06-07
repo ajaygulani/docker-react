237e3f7d0213a39214610f264785b90019b9c6f5
Ajay Gulani <ajaygulani@hotmail.com>
2020-06-07 12:22:06 +1000

    changed text in app.js file

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        Hi
+        I was changed onthe feature branch
         </p>
         <a
           className="App-link"
